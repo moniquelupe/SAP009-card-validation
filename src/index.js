7fb11b85d53a34c5d91ffc9d1bca14f76f080b0a
Monique <monique@Moniques-MacBook-Air.local>
2023-01-23 15:30:29 -0300

    primeiro commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import validator from './validator.js';
 
 console.log(validator);
+function validaCampoNome(){
+    const campoNome = 
+    document.getElementById("nomedotitular");
+
+    campoNome.classList.remove(ïnvalid);
+    let valid = true;
+    console.log('')
+}
