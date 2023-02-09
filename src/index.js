2f4fa4d618977bc402cda300641e8e1268c076e9
Monique Lupe <moniquelupeandrade@gmail.com>
2023-02-09 15:10:42 -0300

    validator

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import validator from './validator.js';
 
-const nmrCartao = document.querySelector("#numero-cartao");
-const btnValidar = document.getElementById("btn-validar");
+const nmrCartao = document.querySelector("#numero-cartao"); ////Vai pegar o valor do input do número do cartão
+const btnValidar = document.getElementById("btn-validar"); //Vai pegar o botão
+const maskedNumbers = validator.maskify(creditCardNumber)
 
 btnValidar.addEventListener("click", function(event) {
     event.preventDefault();
     const texto = nmrCartao.value;
-    validator.isValid(texto);
+    if(validator.isValid(texto)) {
+        return alert("Cartão válido");
+    }
+        return alert("Cartão inválido");
 });
+
+
 
 
 
@@ -18,26 +24,4 @@ passo a passo:
 1. pegar o valor do input
 2. criptografar/validar o número
 3. printar na tela
-
-
-outra forma de escrever o validator:
-import { isValid } from 'validator.js'
-
-
-const cardNumber = document.getElementById("numero-cartao");
-const btnValidar = document.getElementById("btn-validar");
-
-btnValidar.addEventListener("click", function() {
-    alert("Esse cartão é válido!");
-});
-
-console.log("oi");
-
-
-//clicar no botão -> on click - >addEventListener
-
-//element.addEventlistener("cick", myFunction);
-    //element = elemento a ser clicado
-
 */
-
--- src/validator.js
+++ src/validator.js
@@ -1,13 +1,63 @@
 const validator = {
-  isValid: function(numeroCartao) {
-    const numeroCartaoInvertido =  
-    console.log(numeroCartao);
-    for (let i = 0; i <= numeroCartaoInvertido.length -1; i++) {
-      console.log(numeroCartaoInvertido[i]) 
+  maskify(numeroCartao) {
+    const maskNumero = [];
+    for (let i = 0; i < numeroCartao.lenght; i++) {
+      if (i < creditCardNumber.lenght-4) {
+        maskNumber.push('#');
+      } else {
+        maskNumber.push(numeroCartao[i]);
+      }
     }
-
+    return maskNumber.join("");
+      
+    }
   }
+  isValid: function(numeroCartao) {
+    let numeroCartaoInvertido = numeroCartao.split('').reverse(); // guarda o numero do cartao invertido (reverse) como uma string seprada (split)
+    let posPares = [];                                            // guarda as posições pares da string
+    let posImpares = [];                                            // guarda as posições ímpares da string
+    let somaDoisDig = 0;                                          // guarda o valor somado dos digitos dos números maiores que 9
+
+    let somaPares = 0;
+    let somaImpares = 0;
+    let somaValidar = 0;
+  }
+      
+    for (let i = 0; i < numeroCartaoInvertido.length; i++) {
+      if (i % 2 !== 0) {                                           // verifica se o índice é par ou impar
+          
+      if (numeroCartaoInvertido[i]*2 > 9) {                     // verifica se o número é maior que 9
+          somaDoisDig = numeroCartaoInvertido[i]*2;               // guarda o numero no indice "i" multiplicado por 2
+          let temp = somaDoisDig.toString().split('');            // guarda o valor da variavel somaDoisDig como uma string
+          somaDoisDig = parseInt(temp[0]) + parseInt(temp [1]);   // converte cada digito da string em número e soma
+          posPares.push(somaDoisDig);                             // adiciona o numero somado à string das posições pares
+        } else {
+          posPares.push(numeroCartaoInvertido[i]*2);
+        }
+      } else {
+        posImpares.push(parseInt(numeroCartaoInvertido[i]))         // adiciona o número à string das posições pares
+      }
+    }
+    console.log(posPares);
+    console.log(posImpares);
+
+    for (let i = 0; i < posPares.length; i++) {
+      somaPares = somaPares + posPares[i];
+    }
+      
+    for (let i = 0; i < posImpares.length; i++) {
+      somaImpares = somaImpares + posImpares[i];
+
+      
+    somaValidar = somaPares + somaImpares;
+      
    
+    console.log(somaPares)
+    console.log(somaImpares)
+    console.log(somaValidar)
+    console.log(somaValidar % 10 === 0)
+    return somaValidar % 10 === 0
+  }
 };
 
 export default validator;
